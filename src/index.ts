a0766eded772484bb2ec63dba4678b52cfbca223
YangGwangSeong <rhkdtjd_12@naver.com>
2024-11-08 04:56:06 -0800

    feat: request validation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
-
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+new Elysia()
+  .get("/id/:id", () => "Hello World!", {
+    query: t.Object({
+      name: t.String(),
+    }),
+    params: t.Object({
+      id: t.Number(),
+    }),
+  })
+  .listen(3000);
